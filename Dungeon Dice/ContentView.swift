010ef9f7978eff5afa1ab1d622f15d1e065b74c9
realnotben <benjaminfigueroa022@gmail.com>
2026-02-10 10:44:57 -0800

    Initial Commit

---
+++ Dungeon Dice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dungeon Dice
+//
+//  Created by FIGUEROA, BENJAMIN on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dungeon Dice/Dungeon_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DiceApp.swift
+//  Dungeon Dice
+//
+//  Created by FIGUEROA, BENJAMIN on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
